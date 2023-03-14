42bc2489364d262ec3cdc27232f83ce9d176108f
Michał Śmiałowicz <e-mhmz@gft.com>
2023-03-14 11:21:08 +0100

    Update wordcount_df.py

--- pyspark/wordcount_df.py
+++ pyspark/wordcount_df.py
@@ -1,8 +1,10 @@
+import sys
+
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
 
 with SparkSession.builder.config('spark.default.parallelism', '1').getOrCreate() as spark:
-    text_file_df = spark.read.text("../pan-tadeusz.txt")
+    text_file_df = spark.read.text(sys.argv[1])
     counts_df = text_file_df \
         .select(f.explode(f.split("value", " ")).alias("word")) \
         .withColumn("real_words", f.regexp_extract("word", "(\\p{L}+)", 1)) \
@@ -11,4 +13,5 @@ with SparkSession.builder.config('spark.default.parallelism', '1').getOrCreate()
         .select(f.lower(f.col("real_words")).alias("real_words"))\
         .groupBy("real_words") \
         .count()
-    counts_df.write.mode("overwrite").csv("../output")
+    # here repartition(1) doesn't make sense since we have already parallelism set to 1
+    counts_df.repartition(1).write.mode("overwrite").csv(sys.argv[2])
