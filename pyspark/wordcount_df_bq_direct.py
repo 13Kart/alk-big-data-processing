64f2cc6e77f1098ddcee6b85c9c94a8bcea4bcd9
Michał Śmiałowicz <e-mhmz@gft.com>
2023-03-15 11:39:09 +0100

    Add test files

--- pyspark/wordcount_df_bq_direct.py
+++ pyspark/wordcount_df_bq_direct.py
@@ -6,7 +6,7 @@ import pyspark.sql.functions as f
 with SparkSession.builder\
         .config("spark.jars.packages", "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.29.0")\
         .getOrCreate() as spark:
-    # spark.conf.set("temporaryGcsBucket", "gs://alk-big-data-processing-temp")
+    # spark.conf.set("temporaryGcsBucket", "alk-big-data-processing-temp")
     text_file_df = spark.read.text(sys.argv[1])
     counts_df = text_file_df \
         .select(f.explode(f.split("value", " ")).alias("word")) \
