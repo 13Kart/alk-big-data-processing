4555f39d419fbc178ac59cf819dc8b9e444dd97a
Michał Śmiałowicz <e-mhmz@gft.com>
2023-03-15 12:17:40 +0100

    Add creating temp sql view

--- pyspark/gcs_reader.py
+++ pyspark/gcs_reader.py
@@ -3,3 +3,9 @@ from pyspark.sql import SparkSession
 with SparkSession.builder.getOrCreate() as spark:
     users = spark.read.csv("gs://alk-big-data-processing-w1/users")
     users.show()
+    users.createOrReplaceTempView("users")
+    users_vol_2 = spark.sql("""
+    SELECT *
+    FROM users
+    WHERE version = 2
+    """)
