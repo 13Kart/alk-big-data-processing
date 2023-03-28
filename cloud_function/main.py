38f63dcf05a262917ff6db4d81432b10af223b46
Michał Śmiałowicz <e-mhmz@gft.com>
2023-03-28 15:23:58 +0200

    Support env vars for cloud functions

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 
 import functions_framework
 from googleapiclient.discovery import build
@@ -7,21 +8,25 @@ from googleapiclient.discovery import build
 @functions_framework.cloud_event
 def trigger_dataflow(cloud_event):
     dataflow = build("dataflow", "v1b3")
-    project_id = "alk-big-data-processing"
+    project_id = os.environ.get("DATAFLOW_GCP_PROJECT_ID")
+    location = os.environ.get("DATAFLOW_JOB_LOCATION")
+    template_location = os.environ.get("TEMPLATE_LOCATION")
+    temp_location = os.environ.get("TEMP_LOCATION")
+    dataset = os.environ.get("DATASET")
     data = cloud_event.data
     file = data.get("name")
     filename: str = file.split(".")[0]
     request = dataflow.projects().locations().flexTemplates().launch(
         projectId=project_id,
-        location="europe-central2",
+        location=location,
         body={
             "launch_parameter": {
-                "container_spec_gcs_path": "gs://alk-big-data-processing-w3/dataflow-flex-templates/wordcount_bq.json",
-                "job_name": f"wordcount_bq_{datetime.now().strftime('%Y%m%d-%H-%M')}",
+                "container_spec_gcs_path": template_location,
+                "job_name": f"wordcount-bq-{datetime.now().strftime('%Y%m%d-%H-%M')}",
                 "parameters": {
                     "input": f"gs://{data.get('bucket')}/{file}",
-                    "output": f"{project_id}.w3.{filename}",
-                    "temp_location": "gs://alk-big-data-processing-temp/beam-temp"
+                    "output": f"{project_id}.{dataset}.{filename}",
+                    "temp_location": temp_location
                 }
             }
         }
